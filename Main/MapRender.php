33b92a5071bc6aa9779c9cafa5a23577aaae1fb7
Alexei LEXX Zhukov <lexx@go2road.ru>
2019-07-27 15:55:32 +0300

    Добравил класс Рендера для отрисовки модели

--- Main/MapRender.php
+++ Main/MapRender.php
@@ -1,4 +1,8 @@
 <?php
 class MapRender {
-  
+  public function __costruct($config) {
+    $this->config = $config;
+    $this->db = new Db($config['db']);
+
+  }
 }
--- jsonImportWays.php
+++ jsonImportWays.php
@@ -13,11 +13,8 @@ function LatLonLen($lat1,$lon1,$lat2,$lon2) {
   $d = $r * $c; // Distance in km
   return $d;
 }
-/*
-function deg2rad($deg) {
-  return ($deg * (pi()/180));
-}
-*/
+
+
 $objArray = json_decode(file_get_contents("newJSON.json"),true);
 //print_r($objArray);
 for($i=0;$i<Count($objArray["elements"]);$i++) {
@@ -29,9 +26,9 @@ for($i=0;$i<Count($objArray["elements"]);$i++) {
 			if(isset($objArray['elements'][$i]["tags"]["oneway"]) && strtolower($objArray['elements'][$i]["tags"]["oneway"]) == "yes") $oneway = 1;  else $oneway = 0;
 			$node1 = $smartClean->point->get($objArray['elements'][$i]["nodes"][$j]);
 			$node2 = $smartClean->point->get($objArray['elements'][$i]["nodes"][$j+1]);
-			echo LatLonLen($node1["lat"],$node1["lon"],$node2["lat"],$node2["lon"])." ";
+			$len = LatLonLen($node1["lat"],$node1["lon"],$node2["lat"],$node2["lon"]);
 			
-		//	$smartClean->street->add(str_replace(",","",$p1), str_replace(",","",$p2), $oneway);
+			$smartClean->street->add(str_replace(",","",$p1), str_replace(",","",$p2), $oneway, 0,0,$len);
 			
 		}
 	}
