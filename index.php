d00ac2b0fd45b339176851dde8c85803c3b6d9db
Alexei LEXX Zhukov <lexx@webmakerz.ru>
2019-07-27 15:49:52 +0300

    Добавил описания методов

--- index.php
+++ index.php
@@ -7,9 +7,12 @@ $smartClean = new SmartClean(['model' => ['id' => 1]]);
 // -- points
 // $pointID = $smartClean->point->add(OSMid, lat, lon, [height]);
 // $smartClean->point->delete($pointID);
+// $smartClean->point->get($pointID);
 //
 // -- streets
 // $smartClean->street->add(p1, p2, [path_id = 0], [one_way = 0], [width], [len], [priority], [active]);
+// $smartClean->street->delete($pointID);
+// $smartClean->street->get($pointID);
 
 // $smartClean->point->add(87687, 123, 32);
 // $smartClean->point->add(333, 5, 6);
