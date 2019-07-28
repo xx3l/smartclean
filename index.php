e1735419550a208fd7a49cfb5f7f6dee4d933142
vtarak <va.tarakanovsky@ya.ru>
2019-07-28 10:53:01 +0300

    Кнопки больше

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
  <body onload="GetMap();">
     <h2 align="center">Smart Clean</h2>
     <div id="OSMap" style="width:1400px; height:500px;"></div>
-	<button onClick="window.location='index.php?map_id=1&lat=104.2604&lon=52.2621'">Иркутск</button>
-	<button onClick="window.location='index.php?map_id=2&lat=104.0941&lon=52.2118'">Шелехов</button>
+	<button style="width:150px; height:70px; font-size:15pt;" onClick="window.location='index.php?map_id=1&lat=104.2604&lon=52.2621'">Иркутск</button>
+	<button style="width:150px; height:70px; font-size:15pt;" onClick="window.location='index.php?map_id=2&lat=104.0941&lon=52.2118'">Шелехов</button>
 
  </body>
  
