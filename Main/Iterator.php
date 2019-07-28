60b45ee8da5f50bb6c55c3854c7621dc5379ae30
Alexei LEXX Zhukov <lexx@go2road.ru>
2019-07-28 06:34:23 +0300

    Iterator начальные шаги

--- Main/Iterator.php
+++ Main/Iterator.php
@@ -1,6 +1,17 @@
 <?php
 class Iterator extends SmartClean {
   public function __construct() {
-    
+
+  }
+
+  public function init() {
+    // инициализация итератора, копирование конфигурации в память
+    $this->cache[0]['fallout'] = $this->fallout->generate();
+    $this->cache[0]['streets'] = $this->streets->selectAll();
+    $this->cache[0]['transport'] = $this->transport->selectAll();
+  }
+
+  public search($numIterations = 1) {
+    // выполяет серию итераций заданной глубины
   }
 }
